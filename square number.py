ac6f0b6ebfe0dc039dd06d99e13305619d1c46d2
Ray Madachy <madachy@gmail.com>
2022-07-19 10:31:39 -0700

    Create square number.py

---
+++ square number.py
@@ -0,0 +1,6 @@
+# square a number provided by the user and print result
+# input statement gets user input
+# float function converts string input into floating point number for calculations
+number = float(input('What is the number to square? ')) 
+number_squared = number * number
+print(number, 'squared = ', number_squared)
