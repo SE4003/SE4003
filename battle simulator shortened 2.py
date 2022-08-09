477b0adeee863cc6e4dfdc90642ce7629768c4ed
Ray Madachy <madachy@gmail.com>
2022-08-09 07:59:50 -0700

    Add files via upload

---
+++ battle simulator shortened 2.py
@@ -0,0 +1,20 @@
+# inputs
+x0, y0 = 800, 1000
+a, b = .9, .8
+dt = .1
+
+# initializations
+time, x, y = 0, x0, y0
+
+# print output header and starting values
+print (f"{'time':<6} {'x':<6} {'y':<6}") # all columns left justified 6 characters
+print (f"{time:<6.2f} {x:<6.0f} {y:<6.0f}")
+
+# integration of troop levels using Euler's method
+while x > 0 and y > 0: # run until a troop is eliminated
+    time += dt 
+    Δx = -b * y
+    Δy = -a * x
+    x += Δx * dt
+    y += Δy * dt
+    print (f"{time:<6.2f} {max(x, 0):<6.0f} {max(y, 0):<6.0f} {Δx:<6.0f} {Δy:<6.0f}") # max will clip negative end values due to DT approximation
